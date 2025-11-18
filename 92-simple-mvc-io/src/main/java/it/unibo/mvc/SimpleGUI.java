8b8714c3526ec0035062065a3dc870f8c1418938
buldra2000 <buldra00@icloud.com>
2025-11-18 16:26:59 +0100

    Implemented main and display methods

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,10 @@
 package it.unibo.mvc;
 
+import java.io.IOException;
+
 import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +13,25 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 5;   
 
+    public SimpleGUI() {
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+
+        frame.setLocationByPlatform(true);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... args) throws IOException {
+       new SimpleGUI().display();
+       
+    }
+    
 }
