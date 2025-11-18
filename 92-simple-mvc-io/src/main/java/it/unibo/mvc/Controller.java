39106489672f11ad0a2b99e42534b3577f0477de
buldra2000 <buldra00@icloud.com>
2025-11-18 16:11:03 +0100

    Implemented Controller.java without method's body

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,37 @@
 package it.unibo.mvc;
 
+import java.io.File;
 
 /**
  * Application controller. Performs the I/O.
  */
-public class Controller {
+public class Controller implements java.io.Serializable {
+    String PATH = new File(System.getProperty("user.home"))
+        + File.separator
+        + (".txt");
+    
+    File file = new File(PATH);
+
+    //method for setting a File as current file
+    public void setCurrentFile(){
+       
+    }
+
+    //method for getting the current File
+    public void getCurrentFile(){
+        
+    }
+
+    //method for getting the path (in form of String) of the current `File`
+    public String getPath(){
+        return "";
+    }
+
+    //method that gets a `String` as input and saves its content on the current file.
+    //This method may throw an `IOException`.
+    public void saveString(String s){
+    
+    }
+
 
 }
